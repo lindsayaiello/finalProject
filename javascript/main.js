3b37d1a20b9fac5ccb22bb54768b2cba89e232af
lindsayaiello <lr08aiel@gmail.com>
2016-12-11 18:06:56 -0500

    Add files via upload

--- javascript/main.js
+++ javascript/main.js
@@ -1,47 +1,67 @@
-/* CURRENTLY IN: javascript/main.js */
+// CALL FUNCTIONS
 
-// run UI functions
-$(".button-collapse").sideNav();
+sideBar();
+initFirebase();
+authenticateFirebase()
+	.then(function() {
+		getUserInfo();
+		buildAccountData();
+	});
 
-const config = {
-  apiKey: "AIzaSyCa33tJlLm6Ett6mnSR_WB6EUQ8XnrcARE",
-  authDomain: "dogdash-fd9ab.firebaseapp.com",
-  databaseURL: "https://dogdash-fd9ab.firebaseio.com",
-  storageBucket: "dogdash-fd9ab.appspot.com",
-  messagingSenderId: "65389533694"
-};
-firebase.initializeApp(config);
 
-// Sign in using a redirect.
-firebase.auth().getRedirectResult()
-	.then(function(result) {
-		console.log(result)
-	 	var user = result.user;
-	 	// if user doesnt exist, let's log user in
-	  	if (user === null) {
-	  		logInUser();
-	  	}
-	  	else {
-	  		// user IS logged in, let's do other things...
-	  		getUserInfo();
-	  	}
-	})
-	.catch(function(error){
-		// this means no user is logged in - let's log someone in
-		logInUser();
-		
-	})
+// DEFINE FUNCTIONS FOR THIS SITE
 
+// sidebar functionality
+function sideBar(whatToDo) {
+	$(".button-collapse").sideNav(whatToDo);
+}
+
+// initialize firebase
+function initFirebase() {
+	const config = {
+	  apiKey: "AIzaSyCa33tJlLm6Ett6mnSR_WB6EUQ8XnrcARE",
+	  authDomain: "dogdash-fd9ab.firebaseapp.com",
+	  databaseURL: "https://dogdash-fd9ab.firebaseio.com",
+	  storageBucket: "dogdash-fd9ab.appspot.com",
+	  messagingSenderId: "65389533694"
+	};
+	firebase.initializeApp(config);	
+}
+
+
+// log user in
+function authenticateFirebase() {
+	return firebase.auth().getRedirectResult()
+		.then(function(result) {
+			console.log(result)
+		 	var user = result.user;
+		 	// if user doesnt exist, let's log user in
+		  	if (user === null) {
+		  		// logInUser();
+		  	}
+
+		  	return true;
+		})
+		.catch(function(error){
+			// console.log('IN ERROPR')
+			// this means no user is logged in - let's log someone in
+			// logInUser();
+		});
+} // firebaseAuth
+
+// actually log in user
 function logInUser() {
 	var provider = new firebase.auth.FacebookAuthProvider();
 	provider.addScope('user_birthday');
 	firebase.auth().signInWithRedirect(provider);	
 }
 
+// pull in user data
 function getUserInfo() {
 	var user = firebase.auth().currentUser;
 	var name, email, photoUrl, uid;
-
+	console.log('###### USER')
+	console.log(user, user.id)
 	if (user != null) {
 	  name = user.displayName;
 	  email = user.email;
@@ -50,12 +70,39 @@ function getUserInfo() {
 	                   // this value to authenticate with your backend server, if
 	                   // you have one. Use User.getToken() instead.
 	  console.log(name, email, photoUrl)
-	  $('.button-collapse').sideNav('show');
+	  
 
 	  const userImgEl = $('.js-user-img');
 	  userImgEl.attr('src', photoUrl);
+
+	  const userNameEl = $('.js-user-name');
+	  userNameEl.text(name);
+
+	  const userEmailEl = $('.js-user-email');
+	  userEmailEl.text(email);
+
+	  // sideBar('show')
 	  
 	}
+}
+
+function buildAccountData() {
+	var user = firebase.auth().currentUser;
+
+	firebase.database().ref('/users/' + user.uid).once('value')
+		.then(function(snapshot) {
+			return snapshot.val();
+		})
+		.then(function(data){
+			// if nothing in there, user is logging in for first time
+			if (data === null) {
+				
+			}
+			// user has logged in before, load the activity feed
+			else {
+
+			}
+		});
 }
 
 
